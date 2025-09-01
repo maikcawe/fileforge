8142702ffb12b400ebafc257482814157d4a1794
Maik Cawelius <maik.cawelius@icloud.com>
2025-09-01 20:25:33 +0200

    feat: add basic path argument handling

--- cmd/fileforge/main.go
+++ cmd/fileforge/main.go
@@ -1,7 +1,19 @@
 package main
 
 import "fmt"
+import "os"
 
 func main() {
+
 	fmt.Println("Fileforge is alive! 🚀")
+
+	var path string
+
+	if len(os.Args) == 1 {
+		path = "."
+	} else {
+		path = os.Args[1]
+	}
+
+	fmt.Println("Path:", path)
 }
